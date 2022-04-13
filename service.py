4f5961879b6a58cc524b8a955d8aa1db51f6e6f6
BexTuychiev <bex@ibexprogramming.com>
2022-04-13 15:08:04 +0500

    feature: Create the API predict function

--- service.py
+++ service.py
@@ -3,7 +3,7 @@ import warnings
 
 import numpy as np
 import bentoml
-from bentoml.io import Text
+from bentoml.io import Text, NumpyNdarray
 from skimage.transform import resize
 
 
@@ -20,4 +20,20 @@ def create_bento_service_keras(bento_name):
     return keras_model, service
 
 
+model, service = create_bento_service_keras("conv2d_larger_dropout")
 
+
+# Create an API function
+@service.api(input=Text(), output=NumpyNdarray())
+def predict(image_str) -> np.ndarray:
+    """
+    Predict pet pawpularity from an image using the given Bento.
+    """
+    # Convert the image back to numpy array
+    image = np.fromstring(image_str, np.uint8)
+    image = resize(image, (224, 224, 3))
+    image = image / 255.0
+
+    result = model.run(image)
+
+    return result
