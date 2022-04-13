ed1406d0b1d174916c2ad88da418084264fab08a
BexTuychiev <bex@ibexprogramming.com>
2022-04-13 18:16:58 +0500

    feature: Implement the prediction on image inputs

--- src/ui.py
+++ src/ui.py
@@ -1,4 +1,8 @@
 import streamlit as st
+import requests
+import numpy as np
+from PIL import Image
+import io
 
 API_ENDPOINT = "https://pet-pawpularity.herokuapp.com/predict"
 
@@ -16,13 +20,27 @@ def predict(img):
     """
     A function that sends a prediction request to the API and return a cuteness score.
     """
-    pass
+    # Convert the bytes image to a NumPy array
+    bytes_image = img.getvalue()
+    numpy_image_array = np.array(Image.open(io.BytesIO(bytes_image)))
+
+    # Send the image to the API
+    response = requests.post(API_ENDPOINT, headers={"content-type": "text/plain"},
+                             data=str(numpy_image_array))
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        raise Exception("Error: {}".format(response.text))
+
 
 def main():
     img_file = st.file_uploader("Upload an image", type=["jpg", "png"])
 
-    if img_file:
-        st.write("Here's your pet's score!")
+    if img_file is not None:
+        with st.spinner("Predicting..."):
+            prediction = float(predict(img_file).strip("[").strip("]"))
+            st.success(f"Your pet's cuteness score is {prediction:.3f}")
 
     camera_input = st.camera_input("Or take a picture")
 
